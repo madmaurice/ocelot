14122199562673855e205376e83b2c3059a30075
Jonathan Maurice <madmaurice@hotmail.com>
2016-03-20 13:55:39 -0400

    Revert "Test"
    This reverts commit 28be9035ed7698afd4000e5b4c6358439ab19985.

--- sample/BasicScene/main.cpp
+++ sample/BasicScene/main.cpp
@@ -11,10 +11,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
     //Configure debugging stuff
     OC_DBG_CONFIG();
 
-    std::string str("Hello");
-    size_t toto = std::hash<std::string>()(str);
-
-    // TODO : StringHash
-
     return 0;
 }
